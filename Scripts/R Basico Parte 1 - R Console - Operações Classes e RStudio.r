71e76e40befa7624963c5e5d2a90da76f19d37b8
Domingos Napolitano <domingos.napolitano@gmail.com>
2020-08-24 17:56:07 -0300

    Add files via upload

---
+++ Scripts/R Basico Parte 1 - R Console - Operações Classes e RStudio.r
@@ -0,0 +1,147 @@
+1:500
+
+2 * 2 - (4 + 4) / 2
+# adição
+1 + 1
+## [1] 2
+
+# subtração
+4 - 2
+## [1] 2
+
+# multiplicação
+2 * 3
+## [1] 6
+
+# divisão
+5 / 3
+## [1] 1.666667
+
+# potência
+4 ^ 2
+## [1] 16
+
+# resto da divisão de 5 por 3
+5 %% 3
+## [1] 2
+
+# parte inteira da divisão de 5 por 3
+5 %/% 3  
+## [1] 1
+# adição
+1 + 3
+
+# subtração
+4 - 2
+
+# multiplicação
+2 * 3
+
+# divisão
+5 / 3
+
+# potência
+4 ^ 2
+
+# resto da divisão de 5 por 3
+5 %% 3
+
+# parte inteira da divisão de 5 por 3
+5 %/% 3 
+
+7 + 15*3 -7
+
+5 + 15*(3 -7)
+> 5 -
++ 
++ 5
+[1] 0> 5 % 5
+Error: unexpected input in "5 % 5"
+> 5 - 5
+[1] 0 😜a <- 1
+a
+## [1] 1
+a <-1
+
+a
+
+A <- 2
+
+a
+
+A
+# characters
+"a"
+## [1] "a"
+"1"
+## [1] "1"
+"positivo"
+## [1] "positivo"
+"Error: objeto x não encontrado"
+## [1] "Error: objeto x não encontrado"
+"a"
+
+a
+
+A
+
+'A'
+
+'Positivo'
+
+"Error: objeto x não encontrado"
+# numeric
+1
+## [1] 1
+0.10
+## [1] 0.1
+0.95
+## [1] 0.95
+pi
+## [1] 3.141593
+1
+
+0.10
+
+0.95
+
+pi
+# integer
+1L
+## [1] 1
+5L
+## [1] 5
+10L
+## [1] 10
+1L
+
+5L
+
+10L
+
+10
+# complex (raramente utilizado para análise de dados)
+
+2 + 5i
+## [1] 2+5i
+2 + 5i
+`# logical
+
+TRUE   
+[1] TRUE  
+FALSE
+[1] FALSE`
+TRUE
+
+FALSE
+
+x <- 1
+class(x)
+
+y <- "a"
+class(y)
+
+z <- TRUE
+class(z)
+
+
---
+++ Scripts/R Básico Parte 2.r
@@ -0,0 +1,49 @@
+vetor1 <- c(1, 2, 3, 4)
+vetor2 <- c("a", "b", "c")
+
+vetor1
+
+vetor2
+
+class(vetor1)
+
+class(vetor2)
+
+vetor1 - 1
+
+vetor1
+
+vetor1 / 2
+vetor1 * 10
+
+vetor1*vetor1
+
+vetor1
+
+vetor1*(vetor1-1)
+
+vetor2 <- 1:3
+vetor2
+
+vetor1 * vetor2
+
+y <- c(1.7, "a")  # character
+class(y)
+
+y <- c(TRUE, 2)   # numeric
+class(y)
+
+y <- c(TRUE, "a") # character
+class(y)
+
+x <- 0:4
+class(x)
+
+as.numeric(x)
+
+as.logical(x)
+
+as.character(x)
+
+x <- c("a", "b", "c")
+as.numeric(x)
